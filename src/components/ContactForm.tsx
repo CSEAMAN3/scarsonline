d2cdde6961d3519fd9249dd3bcd9395a26b33961
Chris Seaman <christopherseaman1983@gmail.com>
2024-01-21 22:08:00 +0000

    update

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -35,7 +35,7 @@ export default function ContactForm() {
 
     const {name, useremail, message} = formData
 
-    const res = await fetch("http://scarsonline.co.uk/api/resend/send-email", {
+    const res = await fetch("https://scarsonline.co.uk/api/resend/send-email", {
       method: "POST",
       headers: {
         "Content-Type" : "application/json"
